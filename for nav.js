80847554194166ad63ccaf30ac7f2d0b45ffca68
Jonathan <johnjonathan948@gmail.com>
2021-06-22 23:52:45 -1200

    main

--- for nav.js
+++ for nav.js
@@ -10,4 +10,12 @@
       document.getElementById('hidden').style.display = 'none'; 
     }
   }
+
+
+  let serviceIte = document.getElementById('hidd').style.display = 'none';
+  let buttonAl = document.querySelector('.service-butn-1').addEventListener('click', all);
+
+  function all() {
+    document.getElementById('hidd').style.display = 'block'; 
+  }
   
