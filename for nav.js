54ea9cfb22591cb23ce5a4b493f4e2ee6153d556
Jonathan <johnjonathan948@gmail.com>
2021-06-24 00:42:53 -1200

    main

--- for nav.js
+++ for nav.js
@@ -19,3 +19,16 @@
     document.getElementById('hidd').style.display = 'block'; 
   }
   
+
+/*
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  */
