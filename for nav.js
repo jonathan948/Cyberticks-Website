edd16992ce44f071d538e7cb1cc87a8a545fd827
Jonathan <johnjonathan948@gmail.com>
2021-06-28 21:00:24 -1200

    main

--- for nav.js
+++ for nav.js
@@ -1,16 +1,15 @@
 // MOBIL NAVBAR SECTOIN
 
+//FOR MOBILE
+
   let serviceItem = document.getElementById('hidden').style.display = 'none';
   let buttonAll = document.querySelector('.service-butn').addEventListener('click', jj);
 
   function jj() {
-    if (jj) {
     document.getElementById('hidden').style.display = 'block'; 
-    }else{
-      document.getElementById('hidden').style.display = 'none'; 
-    }
   }
 
+//FOR DESKTOP
 
   let serviceIte = document.getElementById('hidd').style.display = 'none';
   let buttonAl = document.querySelector('.service-butn-1').addEventListener('click', all);
