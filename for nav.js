5b5aa9b90e3999ab0edcc7a20aafb4508d173e5c
jonathan948 <75731545+jonathan948@users.noreply.github.com>
2021-07-07 06:13:32 -0700

    main

--- for nav.js
+++ for nav.js
@@ -21,18 +21,6 @@
 
   // Mobile nav
 
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
-  
-
-/*
   function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -43,4 +31,18 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
   }
 
-  */
+
+
+
+
+
+
+
+  // function openNav() {
+  //   document.getElementById("myNav").style.height = "100%";
+  // }
+  
+  // function closeNav() {
+  //   document.getElementById("myNav").style.height = "0%";
+  // }
+
