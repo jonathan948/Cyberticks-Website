dc446d70795a064026296b20d59f48e13d7d1c2f
jonathan948 <75731545+jonathan948@users.noreply.github.com>
2021-07-07 03:09:15 -0700

    main

--- for nav.js
+++ for nav.js
@@ -18,4 +18,17 @@
   function all() {
     document.getElementById('hidd').style.display = 'block'; 
   }
+
+
+  // Mobile nav
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
   
